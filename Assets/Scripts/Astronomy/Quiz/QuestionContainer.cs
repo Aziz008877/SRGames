a68a122602dfe4c758d2909938af281bb3ed5dc5
Aziz008877 <yusifzade1999@gmail.com>
2023-05-29 16:47:35 +0500

    quiz structure

---
+++ Assets/Scripts/Astronomy/Quiz/QuestionContainer.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class QuestionContainer : MonoBehaviour
+{
+    [SerializeField] private List<QuestionData> _allQuestions;
+    [SerializeField] private UnityEvent<QuestionData> _onQuestionSent;
+    [SerializeField] private UnityEvent _onQuizPassed;
+    private int _currentQuestionID = 0;
+
+    private void Start()
+    {
+        SendNewQuestion();
+    }
+
+    private void SendNewQuestion()
+    {
+        _onQuestionSent?.Invoke(_allQuestions[_currentQuestionID]);
+    }
+
+    public void TryIncreaseQuestionData()
+    {
+        if (_currentQuestionID < _allQuestions.Count)
+        {
+            _currentQuestionID++;
+            SendNewQuestion();
+        }
+        else
+        {
+            _onQuizPassed?.Invoke();
+        }
+    }
+}
